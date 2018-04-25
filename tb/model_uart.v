1f790c93ebd3bdb58b1195e0f32d6acabde4bbf5
Leslie Liang <lliang9838@gmail.com>
2018-04-24 22:37:27 -0700

    Finished Nicer UART output, need to run simulation

--- tb/model_uart.v
+++ tb/model_uart.v
@@ -21,6 +21,9 @@ module model_uart(/*AUTOARG*/
    event     evTxByte;
    reg       TX;
 
+   //idea, let's make a buffer, and flushes when we see a carriage return
+   reg [63:0] buff; 
+
    initial
      begin
         TX = 1'b1;
@@ -32,12 +35,23 @@ module model_uart(/*AUTOARG*/
         #(0.5*bittime);
         repeat (8)
           begin
-             #bittime ->evBit;
+             #bittime ->evBit; //need to surpress per byte output
              //rxData[7:0] = {rxData[6:0],RX};
              rxData[7:0] = {RX,rxData[7:1]};
           end
-        ->evByte;
-        $display ("%d %s Received byte %02x (%s)", $stime, name, rxData, rxData);
+        ->evByte; //each byte is 8 bits, let's wait until we get 4 bytes first or 16 bits and then output
+          //let's left shift to make room
+          buff = buff << 8; //left shift by 8 bits to make room for the next byte
+          buff = {buff, rxData};
+
+          if ( rxData == 4'h0D) //we only output when we see a carriage return, then we reset buffer to 0
+          begin
+              $display("Nicer UART output: %s", buff);
+              buff = 0;
+          end
+
+
+        //$display ("%d %s Received byte %02x (%s)", $stime, name, rxData, rxData);
      end
 
    task tskRxData;
